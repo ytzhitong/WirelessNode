44fb6b650b67658528d3e386dde44aa6f466edd3
ytzhitong <ytzhitong@qq.com>
2018-12-20 17:18:40 +0800

    creat WirelessNode pro

---
+++ MAIN/main.c
@@ -0,0 +1,43 @@
+
+#include "sys.h"
+#include "delay.h"
+#include "main.h"
+#include "FreeRTOS.h"
+#include "task.h"
+
+
+//任务优先级
+#define START_TASK_PRIO		1
+//任务堆栈大小	
+#define START_STK_SIZE 		128  
+//任务句柄
+TaskHandle_t StartTask_Handler;
+//任务函数
+void start_task(void *pvParameters);
+
+int main(void)
+{
+	  HAL_Init();
+    SystemClock_Config();	//初始化系统时钟
+    delay_init(); 		    //初始化延时函数   
+
+	//创建开始任务
+    xTaskCreate((TaskFunction_t )start_task,            //任务函数
+                (const char*    )"start_task",          //任务名称
+                (uint16_t       )START_STK_SIZE,        //任务堆栈大小
+                (void*          )NULL,                  //传递给任务函数的参数
+                (UBaseType_t    )START_TASK_PRIO,       //任务优先级
+                (TaskHandle_t*  )&StartTask_Handler);   //任务句柄              
+    vTaskStartScheduler();          //开启任务调度
+}
+
+//开始任务任务函数
+void start_task(void *pvParameters)
+{
+    taskENTER_CRITICAL();           //进入临界区
+
+	
+    vTaskDelete(StartTask_Handler); //删除开始任务
+    taskEXIT_CRITICAL();            //退出临界区
+}
+
---
+++ USER/sys/sys.c
@@ -0,0 +1,126 @@
+#include "sys.h"
+
+/*********************************************************************************
+			  ___   _     _____  _____  _   _  _____  _____  _   __
+			 / _ \ | |   |_   _||  ___|| \ | ||_   _||  ___|| | / /
+			/ /_\ \| |     | |  | |__  |  \| |  | |  | |__  | |/ /
+			|  _  || |     | |  |  __| | . ` |  | |  |  __| |    \
+			| | | || |_____| |_ | |___ | |\  |  | |  | |___ | |\  \
+			\_| |_/\_____/\___/ \____/ \_| \_/  \_/  \____/ \_| \_/
+
+ *	******************************************************************************
+ *	本程序只供学习使用，未经作者许可，不得用于其它任何用途
+ *	ALIENTEK Pandora STM32L475 IOT开发板
+ *	系统时钟初始化
+ *	包括时钟设置/中断管理/GPIO设置等
+ *	正点原子@ALIENTEK
+ *	技术论坛:www.openedv.com
+ *	创建日期:2016/1/5
+ *	版本：V1.0
+ *	版权所有，盗版必究。
+ *	Copyright(C) 广州市星翼电子科技有限公司 2014-2024
+ *	All rights reserved
+ *	******************************************************************************
+ *	版本修改说明
+ *	无
+ *	******************************************************************************/
+
+/**
+ * @brief	时钟系统配置函数
+ *
+ * @remark	SYSCLK = HSE / PLLM * PLLN / PLLR
+ * 			SYSCLK = 8M  /   1  *  20  /2		= 	80M
+ *
+ * @param   void
+ *
+ * @return  void
+ */
+void SystemClock_Config(void)
+{
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+
+  /* Enable HSI Oscillator and Activate PLL with HSI as source */
+  RCC_OscInitStruct.OscillatorType      = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState            = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState        = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource       = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLMUL          = RCC_PLL_MUL6;
+  RCC_OscInitStruct.PLL.PLLDIV          = RCC_PLL_DIV3;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    /* Initialization Error */
+    while(1); 
+  }
+
+  /* Set Voltage scale1 as MCU will run at 32MHz */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+  
+  /* Poll VOSF bit of in PWR_CSR. Wait until it is reset to 0 */
+  while (__HAL_PWR_GET_FLAG(PWR_FLAG_VOS) != RESET) {};
+
+  /* Select PLL as system clock source and configure the HCLK, PCLK1 and PCLK2
+  clocks dividers */
+  RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
+  {
+    /* Initialization Error */
+    while(1); 
+  }
+}
+
+/**
+ * @brief	THUMB指令不支持汇编内联、
+ *			采用如下方法实现执行汇编指令WFI 
+ *
+ * @param   void
+ *
+ * @return  __asm
+ */
+__asm void WFI_SET(void)
+{
+	WFI;		  
+}
+/**
+ * @brief	关闭所有中断(但是不包括fault和NMI中断)
+ *
+ * @param   void
+ *
+ * @return  __asm
+ */
+__asm void INTX_DISABLE(void)
+{
+	CPSID   I
+	BX      LR	  
+}
+/**
+ * @brief	开启所有中断
+ *
+ * @param   void
+ *
+ * @return  __asm
+ */
+__asm void INTX_ENABLE(void)
+{
+	CPSIE   I
+	BX      LR  
+}
+/**
+ * @brief	设置栈顶地址
+ *
+ * @param   addr	栈顶地址
+ *
+ * @return  __asm
+ */
+__asm void MSR_MSP(u32 addr) 
+{
+	MSR MSP, r0 			//set Main Stack value
+	BX r14
+}
+
